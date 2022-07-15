79a39e7957234dad989f6e84a3e283bfbb7da89a
Dmytro.Kabanets <kabanian@live.com>
2022-07-15 17:45:01 +0300

    add section catalog (tabs and flipping cards)

--- src/js/script.js
+++ src/js/script.js
@@ -17,4 +17,25 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__items').removeClass('catalog__items_active').eq($(this).index()).addClass('catalog__items_active');
+    });
+
+    // flipping cards script
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__item-content').eq(i).toggleClass('catalog__item-content_active');
+                $('.catalog__item-list').eq(i).toggleClass('catalog__item-list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog__item-link');
+    toggleSlide('.catalog__item-back');
+
   });
